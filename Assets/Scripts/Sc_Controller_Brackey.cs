33fdd15f377bd9832051fdf9b0488c046723ac92
Tom Mazoyer <71348681+Sebastopole@users.noreply.github.com>
2023-12-05 20:15:38 +0100

    [Controller] [GPP] Tentative de refonte du code du controller

--- Assets/Scripts/Sc_Controller_Brackey.cs
+++ Assets/Scripts/Sc_Controller_Brackey.cs
@@ -10,7 +10,6 @@ public class Sc_Controller_Brackey : MonoBehaviour
     public CharacterController controller;
 
 
-
     public float gravityForce = -9.81f;
     Vector3 velocity;
     public float JumpHeight = 3f;
@@ -21,27 +20,90 @@ public class Sc_Controller_Brackey : MonoBehaviour
     public LayerMask groundMask;
     public bool isGrounded;
     public bool isJumping;
+    public bool isCrounched;
 
     public bool playerLightOn;
     public float playerLightEnergy;
 
 
 
+    public float crouchSpeed = 5f;
+    public float crouchYScale;
+    public float startYScale;
+    public KeyCode crouchKey = LeftControl;
+    
+
+    public MovementState state;
+
+
+
+
+    private void Start()
+    {
+        startYScale = transform.localScale.y; 
+
+
+
+    }
+
+
+
+    public enum MovementState
+    {
+        idle,
+        jumping,
+        walking,
+        crounching,
+    }
+
+    private void StateHandler()
+    {
+        //Mode - Mouvement
+        if(isGrounded && Input.GetAxisRaw("Horizontal") || Input.GetAxisRaw("Vertical"))
+        {
+            state = MouvementState.walking;
+        }
+
+
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
 
+        // Get mouvement input
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
-
-        Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput; //Pour les valeurs du vector3 de direction on prends les inputs et on les associe a leurs directions
-        controller.Move(moveDirection * moveSpeed * Time.deltaTime); //On multiple ces coordonnées par la vitesse de mouvement
-
+        // Mouvement
+        if(MouvementState.walking == true);
+        {
+            Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput; //Pour les valeurs du vector3 de direction on prends les inputs et on les associe a leurs directions
+            controller.Move(moveDirection * moveSpeed * Time.deltaTime); //On multiple ces coordonnées par la vitesse de mouvement
+        }
         
+
+        // Gravité
         velocity.y = velocity.y + gravityForce * Time.deltaTime; //Simulation de la gravité 
         controller.Move(velocity * Time.deltaTime); //Son application sur le controller
+
+
+        //start crouch
+        if(Input.GetKeyDown(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crounchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        //stop crouch
+        if(Input.GetKeyUp(crouchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
+
+
 
 
         isGrounded = Physics.CheckSphere(groundCheck.position, sphereSize, groundMask);
