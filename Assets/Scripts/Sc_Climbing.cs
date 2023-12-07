fea1c8d8b790808c3fca91fe7ed1dce3fac29c23
Tom Mazoyer <71348681+Sebastopole@users.noreply.github.com>
2023-12-07 17:47:08 +0100

    [Controller] [GPP] Polish script Escalade

--- Assets/Scripts/Sc_Climbing.cs
+++ Assets/Scripts/Sc_Climbing.cs
@@ -26,12 +26,12 @@ public class Sc_Climbing : MonoBehaviour
             if((Physics.Raycast(cam.transform.position, cam.transform.forward, out var firstHit, 1f, vaultLayer))) 
             {
                 print("valuable in front");
-                //On lance un ray cast a l'origine du firstHit (contre le mur) + forward le radius du joueur pour pas tombé + Up par la taille du player*0.6 /
+                //On lance un ray cast a l'origine du firstHit (contre le mur) + forward le radius du joueur pour pas tombé + Up par la taille du player*0.6 pour pas qu'il monté partout / ...
+                //... / on tire le ray vers le bas sur le sol puisque c'est la que le joueur bougera 
                 if(Physics.Raycast(firstHit.point + (cam.transform.forward * playerRadius) + (Vector3.up * 0.6f * playerHeight), Vector3.down, out var secondHit, playerHeight))
                 {
                     print("found place to land");
-                    //
-                    StartCoroutine(LerpVault(secondHit.point, 0.5f));
+                    StartCoroutine(LerpVault(secondHit.point, 0.5f)); //On définit la target position et le temps que le joueur mets a monter (duration)
                 }
             }
         }
@@ -42,9 +42,9 @@ public class Sc_Climbing : MonoBehaviour
         float time = 0;
         Vector3 startPosition = transform.position;
 
-        while (time < duration)
+        while (time < duration) //Tant que le temps que le joueur mets a grimper (duration) est supérieur a time, le déplacement est émit
         {
-            transform.position = Vector3.Lerp(startPosition, targetPosition, time/duration);
+            transform.position = Vector3.Lerp(startPosition, targetPosition, time/duration); //On fait bouger le joueur depuis la startingPosition jusqu'a la targetPosition
             time += Time.deltaTime;
             yield return null;
         }
