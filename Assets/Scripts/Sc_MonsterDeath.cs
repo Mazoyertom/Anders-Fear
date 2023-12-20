2c8e45509044f7799e60b5e76ed8fb9f515e783b
Loloma08 <loismaneg@outlook.fr>
2023-12-20 09:32:27 +0100

    Monster Death

---
+++ Assets/Scripts/Sc_MonsterDeath.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_MonsterDeath : MonoBehaviour
+{
+
+    public bool AffectLight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AffectLight)
+        {
+          Destroy(gameObject, 2);   
+        }
+        
+    }
+}
