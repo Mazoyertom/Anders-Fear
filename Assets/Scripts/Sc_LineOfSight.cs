36603b9af3eded91f0efdc5733f18f95bd17e662
Tom Mazoyer <151024690+Mazoyertom@users.noreply.github.com>
2024-01-02 02:44:53 +0100

    [IA][GPP] Au final le fov fonctionne + update prefab

--- Assets/Scripts/Sc_LineOfSight.cs
+++ Assets/Scripts/Sc_LineOfSight.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SC_FOV : MonoBehaviour
+public class Sc_LineOfSight : MonoBehaviour
 {
+
     public LayerMask targetMask;
     public LayerMask obstructionMask;
+
+    public bool seeingThePlayer;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        seeingThePlayer = false;
         
     }
 
@@ -18,4 +23,7 @@ public class SC_FOV : MonoBehaviour
     {
         
     }
+
+
+    
 }
